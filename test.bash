61acd38fe75cececb1f7527803ad4b26a709ad3a
tanaka <s21C1077KF@s.chibakoudai.jp>
2022-10-27 12:39:44 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SODX-FileCopyrightText: 2022 Konomi Tanaka
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,15 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
