b69a78d20445eb3846cfb39ceb76daefaf0f2643
tanaka <s21C1077KF@s.chibakoudai.jp>
2022-11-03 12:25:48 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,26 +1,26 @@
 #!/bin/bash -xv
-# SODX-FileCopyrightText: 2022 Konomi Tanaka
+# SPDX-FileCopyrightText: 2022 Konomi Tanaka
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng ( ) {
 	echo NG at Line $1
-		res=1
-	}
+	res=1
+}
 
-	res=0
+res=0
 
-	### I/O TEST ###
-	out=$(seq 5 | ./plus)
-	[ "${out}" = 15 ] || ng ${LINENO}
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-	### STRANGE INPUT ###
-	out=$(echo あ | ./plus)
-	[ "$?" = 1 ]      || ng ${LINENO}
-	[ "${out}" = "" ] || ng ${LINENO}
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-	out=$(echo | ./plus)
-	[ "$?" = 1 ]      || ng ${LINENO}
-	[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-	[ "$res" = 0 ] && echo OK
-	exit $res
+[ "$res" = 0 ] && echo OK
+exit $res
